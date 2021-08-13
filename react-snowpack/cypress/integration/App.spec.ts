463fb507958d641d6ad558135c9e9bd10cb9cbfb
Barnes, Andrew <andrew.barnes@troweprice.com>
2021-08-13 15:31:53 +0100

    Follow links in cypress App spec

--- react-snowpack/cypress/integration/App.spec.ts
+++ react-snowpack/cypress/integration/App.spec.ts
@@ -6,10 +6,18 @@ describe('App', () => {
   it('Has Home link', () => {
     cy.get('a[href="/"]')
       .should('have.text', 'Home')
+      .click()
+
+    cy.url()
+      .should('equal', `${Cypress.config().baseUrl}/`)
   })
 
   it('Has Message link', () => {
     cy.get('a[href="/message"]')
       .should('have.text', 'Message')
+      .click()
+
+    cy.url()
+      .should('equal', `${Cypress.config().baseUrl}/message`)
   })
 })
